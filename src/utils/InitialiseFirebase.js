3eb359f304def43bd0cdee3563d9352dc8b5f2bd
Kieran C <kieranakc2@gmail.com>
2021-09-25 14:23:24 +0100

    env file initialize app

--- src/utils/InitialiseFirebase.js
+++ src/utils/InitialiseFirebase.js
@@ -2,19 +2,6 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-// firebase.initializeApp({
-//   apiKey: 'AIzaSyA8Qq49hTm_WtRiaaddA2S91JX09sTLKfc',
-//   authDomain: 'realtime-react-chat-app.firebaseapp.com',
-//   databaseURL:
-//     'https://realtime-react-chat-app-default-rtdb.europe-west1.firebasedatabase.app',
-//   projectId: 'realtime-react-chat-app',
-//   storageBucket: 'realtime-react-chat-app.appspot.com',
-//   messagingSenderId: '288793514543',
-//   appId: '1:288793514543:web:b3efa82dcd4f0fa1e1acda',
-// });
-
-console.log(process.env);
-
 firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
