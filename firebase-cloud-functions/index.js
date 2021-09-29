f713b004b51ba1058a1ca1b481436a27ccbe1d09
Kieran C <kieranakc2@gmail.com>
2021-09-29 21:42:07 +0100

    firebase cloud functions updated

---
+++ firebase-cloud-functions/index.js
@@ -0,0 +1,36 @@
+const Filter = require('bad-words');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+const db = admin.firestore();
+
+exports.filterBadWords = functions.firestore
+  .document('messages/{docID}')
+  .onCreate(async (doc, context) => {
+    const filter = new Filter();
+    const { message } = doc.data();
+
+    if (filter.isProfane(message)) {
+      await doc.ref.update({
+        message: '[message was deleted for profanity 🤐]',
+      });
+    }
+  });
+
+exports.incrementUserWrites = functions.firestore
+  .document('messages/{docID}')
+  .onCreate(async (doc, context) => {
+    const { uid } = doc.data();
+    const uidDoc = await db.collection('users').doc(uid).get();
+
+    if (uidDoc.exists) {
+      let payload =
+        uidDoc.data().writes >= 50
+          ? { banned: true }
+          : { writes: admin.firestore.FieldValue.increment(1) };
+      await uidDoc.ref.update(payload);
+    } else {
+      await uidDoc.ref.set({ writes: 1, banned: false });
+    }
+  });
